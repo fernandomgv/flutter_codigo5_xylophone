05462a6e60ea4e301d88e938fbdde533cc7c8aea
fernandomgv <fernando.mogollon@iica.int>
2022-04-22 21:37:09 -0500

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("XylophoneApp"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+
+          },
+          child: Text("Click!!"),
+        ),
+      ),
+    );
+  }
+}
+
+
+
